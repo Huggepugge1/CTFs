3f26f3ad79bbd8a92ff3515d47a8a5f6e2a253a8
Hugo Lindström <huggepugge1@gmail.com>
2022-04-19 12:18:33 +0200

    removed crew

--- Crew/2022/email/install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ninoseki/eml_analyzer.git
-cd eml_analyzer
-docker build . -t eml_analyzer
-docker run -i -d -p 8000:8000 eml_analyzer
--- Crew/2022/waldo/dataset/count.sh
+++
@@ -1,6 +0,0 @@
-while true 
-do
-	clear
-	ls dataset | wc -l
-	sleep 1
-done
