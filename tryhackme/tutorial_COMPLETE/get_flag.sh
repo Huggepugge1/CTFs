0b6c1c816a12f980c38fc9de4d39aa1a3b23f8d0
Hugo Lindström <huggepugge1@gmail.com>
2023-02-03 23:55:20 +0100

    bandit

--- tryhackme/tutorial_COMPLETE/get_flag.sh
+++ tryhackme/tutorial_COMPLETE/get_flag.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 source ~/.zshrc
-curl 10.10.64.140 -s | grep -oE "flag{.*}" --color=none
+
+curl $1 -s | grep -oE "flag{.*}" --color=none
---
+++ tuctf/leisurely_math/solve.py
@@ -0,0 +1,14 @@
+gspn qxo jnqpsu  *
+
+t = sfnpuf('dibmt.uvdug.dpn', 30202)
+
+gps j jo sbohf(633):
+    bot = tus(fwbm(t.sfdwmjof()[:-1]))
+    t.sfdwvoujm(c': ')
+    t.tfoemjof(czuft(bot, 'vug-8'))
+    sftq = t.sfdwmjof()
+    qsjou(j)
+
+t.joufsbdujwf()
+
+t.dmptf()
