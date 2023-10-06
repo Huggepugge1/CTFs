7180de27d47eb87925ad67e243e9bab8cf38d279
hugo Lindström <huggepugge1@gmail.com>
2023-10-06 10:04:29 +0200

    bashsave.sh update, printf

--- savescripts/bashsave.sh
+++ savescripts/bashsave.sh
@@ -4,10 +4,12 @@ if [ $# -eq 1 ]
 	then nu=$1
 fi
 
-echo "#!/bin/bash
+printf "#!/bin/bash
 shopt -s expand_aliases
 source ~/.bashrc
 $(history | cut -b 8- | tail -n$((nu+1)) | head -n$((nu)))" > get_flag.sh
 
 chmod +x get_flag.sh
 source ./get_flag.sh > flag.txt
+
+printf "### Flag\nFlag: \`$(cat flag.txt)\`" >> README.md
